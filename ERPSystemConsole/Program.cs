774f66a1186d7c34e13b97f904f34a55d959b31f
Sarah <164634250+xdbellamy@users.noreply.github.com>
2024-11-27 12:52:27 +0100

    Add project files.

---
+++ ERPSystemConsole/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ERPSystemConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
